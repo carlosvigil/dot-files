859f0111bd08657fa8950bb9acda3d3e23aae297
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2023-05-26 16:28:51 -0400

    update

--- zprofile.zsh
+++ zprofile.zsh
@@ -1,5 +1,3 @@
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
 export EDITOR='nvim'
 
 NEWLINE=$'\n'
@@ -14,9 +12,3 @@ alias cvnet="cd ~/Desktop/carlos.network"
 alias fcc="cd ~/Desktop/fcc-projects"
 
 alias updateAll="echo '\nUpdating and cleaning Homebrew\n' && brew update && brew update && brew upgrade && brew cleanup && brew prune && echo '\nUpdating and cleaning RubyGems\n' && gem update --system && gem update && gem cleanup && echo '\nUpdating and cleaning NodePackageManager\n' && npm -g outdated && npm -g update && npm -g upgrade"
-
-
-# open tmux on remote
-ssh () {/usr/bin/ssh -t "$@" tmux new -A}
-# specific to sleepy-giraffes
-alias sleepy="mosh sleepy-giraffes -- tmux new -A sesh"
