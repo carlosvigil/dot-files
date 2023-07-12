39958cdf42d8be97fae679afa9b8f4d18279fa10
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2023-07-11 21:09:32 -0400

    switch to zsh

---
+++ aliases.zsh
@@ -0,0 +1,12 @@
+# See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
+# 
+# Files in the custom/ directory will be:
+# - loaded automatically by the init script, in alphabetical order
+# - loaded last, after all built-ins in the lib/ directory, to override them
+# - ignored by git by default
+
+alias pn=pnpm
+alias v=nvim
+alias vi=nvim
+alias desk='cd ~/Desktop'
+alias mellc='cd ~/MELLC-metarepo'
