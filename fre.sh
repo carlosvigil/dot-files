a4cf80782df4c7b6b0a5be25f72e8117bb822f9f
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2023-09-11 19:44:56 -0400

    Update fresh install script

--- fre.sh
+++ fre.sh
@@ -1,28 +1,54 @@
-#!/bin/bash
-# TODO: Copy packages into a nix Flake
-# oldTODO: duplicate and translate for linux containers
-# NOTE: 
+#!/usr/bin/env dash
 # FOR A FRESH MAC INSTALL
+# TODO: check for linux containers, and install into them
+# TODO: add dot-files
+# TODO: add new vim installation tasks, lazy vim
+# TODO: add post-brew-install tasks, i.e. echoes into zshrc
+
 T=$(date +"%x %r %Z")
 arg=$1
 
-get_brew() {
+brews = (asdf
+	bat 
+	git 
+	gh 
+	ripgrep 
+	lf 
+	helix 
+	neovim)
+
+casks = (warp 
+	visual-studio-code 
+	betterdisplay 
+	soundsource 
+	orbstack 
+	utm 
+	fontbase 
+	numi 
+	spotify 
+	vlc 
+	appcleaner 
+	suspicious-package)
+
+install_brew() {
   echo "Getting BREW and some formulae..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   echo "Doctor..."
   brew doctor
   echo "Update 2x..."
   brew update && brew update
-  echo "Installing: bat, helix, nvim, git, gh, ag, ranger, curl"
-  brew install bat helix neovim git gh ag ranger curl
-  echo "Installing casks: soundsource, vlc, vscode, fontbase, and numi..."
-  brew install --cask soundsource vlc visual-studio-code fontbase numi
+  echo "Installing brews..."
+  brew install $brews
+  echo "Installing casks..."
+  brew install --cask $casks
   echo "Installing patched font..."
-  brew tap homebrew/cask-fonts
+  brew tap homebrew/cask-fonts; \
   brew install font-victor-mono-nerd-font
   echo "Done brewing."
   return
 }
+
+
 
 get_vim_plug() {
   echo "Getting VIMPLUG..."
@@ -33,7 +59,6 @@ get_vim_plug() {
 start() {
   echo "STARTING [$T]"
   get_brew
-  get_vim_plug
   echo "All done."
   echo [$T]
 }
