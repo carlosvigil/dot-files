09843b855b31869a6c6a45a25ddfc8823cf11053
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2023-12-10 02:54:23 -0500

    Update shell aliases; correct global git ignore; add /Users/c ignore

--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,14 @@
-# See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
-# 
-# Files in the custom/ directory will be:
-# - loaded automatically by the init script, in alphabetical order
-# - loaded last, after all built-ins in the lib/ directory, to override them
-# - ignored by git by default
 alias x=exit
+alias \.='cd $HOME'
+alias \.\.='cd ..'
 
-alias pn=pnpm
+alias p=pnpm
 alias v=nvim
-alias vi=nvim
+alias h=hx
 
 alias conf='cd $HOME/.config'
 alias desk='cd $HOME/Desktop'
-alias mellc='cd $HOME/src/MELLC-metarepo'
+alias mellc='cd $HOME/local/MELLC-metarepo'
 
 alias nvim_hard_reset='rm -rf ~/.cache/nvim ~/.local/share/nvim/ ~/.local/state/nvim/ ~/.config/nvim/'
+alias nvim_cp_plugs='cp ~/.config/nvim/lua/plugins.lua ~/dot-files/nvim/lua/plugins.lua'
